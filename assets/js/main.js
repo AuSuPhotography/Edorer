016ef21b0db6533dddec3e244b68fcef443cc879
Abhishek Gupta <abhishek251gupta@gmail.com>
2021-03-25 15:09:50 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,160 @@
-function ajax(e){const t=new XMLHttpRequest;t.open(e.method,e.url,e.data),t.send(),t.onload=(()=>{200!==t.status||e.success(t.response)}),t.onerror=e.error()}function imgUpload(){document.getElementById("imgUpload").addEventListener("click",e=>{console.log(e.currentTarget),document.getElementById("selectImg").click()})}function addTabChange(){let e;const t={pic1:"./assets/img/Group144.png",pic2:"./Images/power.png",pic3:"./Images/Rectangle 230.png",pic4:"./Images/WAC-Logo-Web 1.png",pic5:"./Images/Group 150@2x.png"};document.getElementsByName("divHead").forEach((a,n)=>{a.style.cursor="pointer",0===n&&((e=a).style.borderTop="4px solid #EC4056",e.getElementsByTagName("h6")[0].style.color="#EC4056"),a.addEventListener("click",a=>{document.getElementById("showPic").src=t[a.currentTarget.id],e.style.borderTop="4px solid #19071c10",e.getElementsByTagName("h6")[0].style.color="#19071C",a.currentTarget.style.borderTop="4px solid #EC4056",a.currentTarget.style.transition="all 0.5s ease-out 0s",a.currentTarget.getElementsByTagName("h6")[0].style.color="#EC4056",e=a.currentTarget})})}function addAccordian(){let e;document.getElementsByName("graphChange").forEach((t,a)=>{0===a&&(t.classList.add("accordian-card-height"),t.getElementsByClassName("heading")[0].childNodes[1].style.color=t.getElementsByClassName("heading")[0].getElementsByTagName("h6")[0].getElementsByTagName("span")[0].style.color="#EC4056",t.getElementsByClassName("heading")[0].childNodes[1].style.opacity=t.getElementsByClassName("heading")[0].getElementsByTagName("h6")[0].getElementsByTagName("span")[0].style.opacity=1,t.getElementsByClassName("heading")[0].childNodes[1].style.display="block",t.getElementsByClassName("para")[0].classList.remove("graph-img"),e={element:t,c:a+1},document.getElementById(`timeline-${e.c}`).style.display="block",window.screen.availWidth>=768&&window.screen.availWidth<1366&&(document.getElementById(`graph${e.c}`).style.display="flex")),t.addEventListener("click",t=>{const n=a+1;e.c!==n&&(t.currentTarget.classList.add("accordian-card-height"),t.currentTarget.getElementsByClassName("heading")[0].childNodes[1].style.color=t.currentTarget.getElementsByClassName("heading")[0].getElementsByTagName("h6")[0].getElementsByTagName("span")[0].style.color="#EC4056",t.currentTarget.getElementsByClassName("heading")[0].childNodes[1].style.opacity=t.currentTarget.getElementsByClassName("heading")[0].getElementsByTagName("h6")[0].getElementsByTagName("span")[0].style.opacity=1,t.currentTarget.getElementsByClassName("heading")[0].childNodes[1].style.display="block",t.currentTarget.getElementsByClassName("para")[0].classList.remove("graph-img"),document.getElementById(`timeline-${e.c}`).style.display="none",document.getElementById(`timeline-${n}`).style.display="block",window.screen.availWidth>=768&&window.screen.availWidth<=1366&&(document.getElementById(`graph${e.c}`).style.display="none",document.getElementById(`graph${n}`).style.display="flex"),e.element.getElementsByClassName("heading")[0].childNodes[1].style.color=e.element.getElementsByClassName("heading")[0].getElementsByTagName("h6")[0].getElementsByTagName("span")[0].style.color="#19071C",e.element.getElementsByClassName("heading")[0].childNodes[1].style.opacity=1,e.element.getElementsByClassName("heading")[0].getElementsByTagName("h6")[0].getElementsByTagName("span")[0].style.opacity=.1,e.element.getElementsByClassName("heading")[0].childNodes[1].style.display="inline-flex",e.element.getElementsByClassName("para")[0].classList.add("graph-img"),e.element.classList.remove("accordian-card-height"),e.element=t.currentTarget,e.c=n)})})}function addAnimations(){var e=document.getElementsByClassName("slide-btn"),t=document.getElementById("slide");for(let a in e)e[a].onclick=function(){for(t.style.transform=`translateX(-${100*a}%)`,j=0;j<4;j++)e[j].classList.remove("active");this.classList.add("active")}}function removeAnimations(){document.getElementsByName("graphChange").forEach((e,t)=>{e.removeEventListener("click",()=>{})}),document.getElementsByName("divHead").forEach((e,t)=>{e.removeEventListener("click",()=>{e.style.border="none"})})}window.addEventListener("resize",()=>{window.screen.availWidth<768?removeAnimations():window.screen.availWidth>768&&(addAccordian(),addTabChange())}),imgUpload(),addAnimations(),window.screen.availWidth<766||(addAccordian(),addTabChange()),document.getElementById("myBtn").addEventListener("click",function(e){e.preventDefault(),document.getElementById("myModal").style.display="block"}),document.getElementsByClassName("close").addEventListener("click",function(e){e.preventDefault(),document.getElementById("myModal").style.display="none"});function ajax(e){const t=new XMLHttpRequest;t.open(e.method,e.url,e.data),t.send(),t.onload=(()=>{200!==t.status||e.success(t.response)}),t.onerror=e.error()}function imgUpload(){document.getElementById("imgUpload").addEventListener("click",e=>{console.log(e.currentTarget),document.getElementById("selectImg").click()})}function addTabChange(){let e;const t={pic1:"./assets/img/Group144.png",pic2:"./Images/power.png",pic3:"./Images/Rectangle 230.png",pic4:"./Images/WAC-Logo-Web 1.png",pic5:"./Images/Group 150@2x.png"};document.getElementsByName("divHead").forEach((a,n)=>{a.style.cursor="pointer",0===n&&((e=a).style.borderTop="4px solid #EC4056",e.getElementsByTagName("h6")[0].style.color="#EC4056"),a.addEventListener("click",a=>{document.getElementById("showPic").src=t[a.currentTarget.id],e.style.borderTop="4px solid #19071c10",e.getElementsByTagName("h6")[0].style.color="#19071C",a.currentTarget.style.borderTop="4px solid #EC4056",a.currentTarget.style.transition="all 0.5s ease-out 0s",a.currentTarget.getElementsByTagName("h6")[0].style.color="#EC4056",e=a.currentTarget})})}function addAccordian(){let e;document.getElementsByName("graphChange").forEach((t,a)=>{0===a&&(t.classList.add("accordian-card-height"),t.getElementsByClassName("heading")[0].childNodes[1].style.color=t.getElementsByClassName("heading")[0].getElementsByTagName("h6")[0].getElementsByTagName("span")[0].style.color="#EC4056",t.getElementsByClassName("heading")[0].childNodes[1].style.opacity=t.getElementsByClassName("heading")[0].getElementsByTagName("h6")[0].getElementsByTagName("span")[0].style.opacity=1,t.getElementsByClassName("heading")[0].childNodes[1].style.display="block",t.getElementsByClassName("para")[0].classList.remove("graph-img"),e={element:t,c:a+1},document.getElementById(`timeline-${e.c}`).style.display="block",window.screen.availWidth>=768&&window.screen.availWidth<1366&&(document.getElementById(`graph${e.c}`).style.display="flex")),t.addEventListener("click",t=>{const n=a+1;e.c!==n&&(t.currentTarget.classList.add("accordian-card-height"),t.currentTarget.getElementsByClassName("heading")[0].childNodes[1].style.color=t.currentTarget.getElementsByClassName("heading")[0].getElementsByTagName("h6")[0].getElementsByTagName("span")[0].style.color="#EC4056",t.currentTarget.getElementsByClassName("heading")[0].childNodes[1].style.opacity=t.currentTarget.getElementsByClassName("heading")[0].getElementsByTagName("h6")[0].getElementsByTagName("span")[0].style.opacity=1,t.currentTarget.getElementsByClassName("heading")[0].childNodes[1].style.display="block",t.currentTarget.getElementsByClassName("para")[0].classList.remove("graph-img"),document.getElementById(`timeline-${e.c}`).style.display="none",document.getElementById(`timeline-${n}`).style.display="block",window.screen.availWidth>=768&&window.screen.availWidth<=1366&&(document.getElementById(`graph${e.c}`).style.display="none",document.getElementById(`graph${n}`).style.display="flex"),e.element.getElementsByClassName("heading")[0].childNodes[1].style.color=e.element.getElementsByClassName("heading")[0].getElementsByTagName("h6")[0].getElementsByTagName("span")[0].style.color="#19071C",e.element.getElementsByClassName("heading")[0].childNodes[1].style.opacity=1,e.element.getElementsByClassName("heading")[0].getElementsByTagName("h6")[0].getElementsByTagName("span")[0].style.opacity=.1,e.element.getElementsByClassName("heading")[0].childNodes[1].style.display="inline-flex",e.element.getElementsByClassName("para")[0].classList.add("graph-img"),e.element.classList.remove("accordian-card-height"),e.element=t.currentTarget,e.c=n)})})}function addAnimations(){var e=document.getElementsByClassName("slide-btn"),t=document.getElementById("slide");for(let a in e)e[a].onclick=function(){for(t.style.transform=`translateX(-${100*a}%)`,j=0;j<4;j++)e[j].classList.remove("active");this.classList.add("active")}}function removeAnimations(){document.getElementsByName("graphChange").forEach((e,t)=>{e.removeEventListener("click",()=>{})}),document.getElementsByName("divHead").forEach((e,t)=>{e.removeEventListener("click",()=>{e.style.border="none"})})}window.addEventListener("resize",()=>{window.screen.availWidth<768?removeAnimations():window.screen.availWidth>768&&(addAccordian(),addTabChange())}),imgUpload(),addAnimations(),window.screen.availWidth<766||(addAccordian(),addTabChange()),document.getElementById("myBtn").addEventListener("click",function(e){e.preventDefault(),document.getElementById("myModal").style.display="block"}),document.getElementsByClassName("close").addEventListener("click",function(e){e.preventDefault(),document.getElementById("myModal").style.display="none"});
+window.addEventListener('resize', () => {
+    if(window.screen.availWidth < 768)removeAnimations();
+    else if(window.screen.availWidth > 768){
+      addAccordian();
+      addTabChange();
+    }
+  });
+
+function ajax(options) {
+  const xhr = new XMLHttpRequest();
+  xhr.open(options.method, options.url, options.data);
+  xhr.send();
+  xhr.onload = () => {
+      if(xhr.status !== 200){
+
+      }else{
+          options.success(xhr.response);
+      }
+  }
+  xhr.onerror = options.error();
+}
+
+function imgUpload(){
+  document.getElementById('imgUpload').addEventListener('click', event => {
+    console.log(event.currentTarget);
+    document.getElementById('selectImg').click();
+  });
+}
+
+
+function addTabChange(){
+  let lastEleForTab;
+  const option = {
+    pic1: './assets/img/Group144.png',
+    pic2: './Images/power.png',
+    pic3: './Images/Rectangle 230.png',
+    pic4: './Images/WAC-Logo-Web 1.png',
+    pic5: './Images/Group 150@2x.png',
+  };
+
+  document.getElementsByName("divHead").forEach((element, index) => {
+    element.style.cursor = 'pointer';
+    if(index === 0){
+      lastEleForTab = element;
+      lastEleForTab.style.borderTop = '4px solid #EC4056';
+      lastEleForTab.getElementsByTagName('h6')[0].style.color = '#EC4056';
+    }
+    element.addEventListener('click', (event) => {
+      document.getElementById('showPic').src = option[event.currentTarget.id];
+      lastEleForTab.style.borderTop = '4px solid #19071c10';
+      lastEleForTab.getElementsByTagName('h6')[0].style.color = '#19071C';
+
+      event.currentTarget.style.borderTop = '4px solid #EC4056';
+      event.currentTarget.style.transition = 'all 0.5s ease-out 0s';
+      event.currentTarget.getElementsByTagName('h6')[0].style.color = '#EC4056';
+      lastEleForTab = event.currentTarget;
+    });
+  });
+}
+
+function addAccordian(){
+  let lastEleForDiv;
+  document.getElementsByName("graphChange").forEach((element, index) => {
+    if(index === 0){
+      element.classList.add('accordian-card-height');
+      element.getElementsByClassName('heading')[0].childNodes[1].style.color =
+      element.getElementsByClassName('heading')[0].getElementsByTagName('h6')[0].getElementsByTagName('span')[0].style.color = '#EC4056';
+      element.getElementsByClassName('heading')[0].childNodes[1].style.opacity =
+      element.getElementsByClassName('heading')[0].getElementsByTagName('h6')[0].getElementsByTagName('span')[0].style.opacity = 1;
+      element.getElementsByClassName('heading')[0].childNodes[1].style.display = 'block';
+      element.getElementsByClassName('para')[0].classList.remove('graph-img');
+      lastEleForDiv = {
+        element,
+        c: index + 1
+      };
+      document.getElementById(`timeline-${lastEleForDiv.c}`).style.display = 'block';
+
+      if(window.screen.availWidth >= 768 && window.screen.availWidth < 1366){
+        document.getElementById(`graph${lastEleForDiv.c}`).style.display = 'flex';
+      }
+    }
+
+    element.addEventListener('click', (event) => {
+        const num = index + 1;
+        if(lastEleForDiv.c === num) return;
+        event.currentTarget.classList.add('accordian-card-height');
+        event.currentTarget.getElementsByClassName('heading')[0].childNodes[1].style.color =
+        event.currentTarget.getElementsByClassName('heading')[0].getElementsByTagName('h6')[0].getElementsByTagName('span')[0].style.color = '#EC4056';
+        event.currentTarget.getElementsByClassName('heading')[0].childNodes[1].style.opacity =
+        event.currentTarget.getElementsByClassName('heading')[0].getElementsByTagName('h6')[0].getElementsByTagName('span')[0].style.opacity = 1;
+        event.currentTarget.getElementsByClassName('heading')[0].childNodes[1].style.display = 'block';
+        event.currentTarget.getElementsByClassName('para')[0].classList.remove('graph-img');
+
+        document.getElementById(`timeline-${lastEleForDiv.c}`).style.display = 'none';
+        document.getElementById(`timeline-${num}`).style.display = 'block';
+
+        if(window.screen.availWidth >= 768 && window.screen.availWidth <= 1366){
+          document.getElementById(`graph${lastEleForDiv.c}`).style.display = 'none';
+          document.getElementById(`graph${num}`).style.display = 'flex';
+        }
+
+        lastEleForDiv.element.getElementsByClassName('heading')[0].childNodes[1].style.color =
+        lastEleForDiv.element.getElementsByClassName('heading')[0].getElementsByTagName('h6')[0].getElementsByTagName('span')[0].style.color = '#19071C';
+        lastEleForDiv.element.getElementsByClassName('heading')[0].childNodes[1].style.opacity = 1;
+        lastEleForDiv.element.getElementsByClassName('heading')[0].getElementsByTagName('h6')[0].getElementsByTagName('span')[0].style.opacity = 0.1;
+        lastEleForDiv.element.getElementsByClassName('heading')[0].childNodes[1].style.display = 'inline-flex';
+        lastEleForDiv.element.getElementsByClassName('para')[0].classList.add('graph-img');
+        lastEleForDiv.element.classList.remove('accordian-card-height')
+        lastEleForDiv.element = event.currentTarget;
+        lastEleForDiv.c = num;
+    })
+  });
+}
+
+function addAnimations(){
+  var slideBtn = document.getElementsByClassName('slide-btn');
+  var slide = document.getElementById("slide");
+  for(let i in slideBtn){
+    slideBtn[i].onclick = function(){
+      slide.style.transform = `translateX(-${100*i}%)`;
+      for(j=0; j<4; j++) {
+        slideBtn[j].classList.remove('active');
+      } this.classList.add('active');
+    };
+  }
+}
+
+
+function removeAnimations(){
+  document.getElementsByName('graphChange').forEach((element, index) => {
+    element.removeEventListener('click', () => {
+
+    });
+  });
+
+  document.getElementsByName('divHead').forEach((element, index) => {
+    element.removeEventListener('click', () => {
+      element.style.border = 'none';
+    });
+  });
+}
+
+
+(function(){
+  imgUpload();
+  addAnimations();
+  if(window.screen.availWidth < 766) return;
+  addAccordian();
+  addTabChange();
+})();
+
+document.getElementById("myBtn").addEventListener('click', function(event){
+  event.preventDefault()
+  document.getElementById("myModal").style.display = 'block';
+});
+console.log(document.getElementById("close"))
+document.getElementsById("close").addEventListener('click', function(event){
+  event.preventDefault()
+  document.getElementById("myModal").style.display = 'none';
+});
